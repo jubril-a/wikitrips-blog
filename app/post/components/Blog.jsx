42b4207de14e1c0b7218383b198b86e30c5bceec
Jubril <abdulsalamjubr3@gmail.com>
2025-05-13 09:40:51 +0100

    fix deployment issue

--- app/post/components/Blog.jsx
+++ app/post/components/Blog.jsx
@@ -1,7 +1,54 @@
+// 'use client'
+
+// import { createClient } from "next-sanity"
+// import { useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import Header from "./Header";
+// import Body from "./Body";
+
+// const client = createClient({
+//     apiVersion: "2025-05-08",
+//     dataset: "production",
+//     projectId: "8zi7n528",
+//     useCdn: false,
+// })
+
+
+// const options = { next: { revalidate: 30 } };
+
+// export default function Blog() {
+//     const urlParams = useSearchParams()
+//     const postId = urlParams.get("post_id")
+//     const [post, setPost] = useState(null)
+
+//     useEffect(() => {
+//         const POSTS_QUERY = `*[_type == "post" && _id == "${postId}"][0]{title, short, publishedAt, ...mainImage{...asset-> {url}}, body}`
+
+//         const fetchedPost = async () => {
+//             let pst = await client.fetch(POSTS_QUERY, {}, options);
+
+//             setPost(pst)
+//         }
+
+
+//         fetchedPost()
+
+//     }, [postId])
+
+//     return (
+//         <div className="max-w-[680px] m-auto mt-9">
+//             <Header h1={post.title} description={post.short} imgUrl={post.url}  />
+//             <Body content={post.body} />
+//         </div>
+//     )
+// }
+
+
 'use client'
 
 import { createClient } from "next-sanity"
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Body from "./Body";
 
@@ -12,23 +59,35 @@ const client = createClient({
     useCdn: false,
 })
 
+export default function Blog() {
+    const searchParams = useSearchParams()
+    const postId = searchParams.get("post_id")
+    const [post, setPost] = useState(null)
 
-const options = { next: { revalidate: 30 } };
+    useEffect(() => {
+        if (!postId) return;
 
-export default async function Blog() {
+        const fetchPost = async () => {
+            const POSTS_QUERY = `*[_type == "post" && _id == "${postId}"][0]{title, short, publishedAt, mainImage{asset->{url}}, body}`
+            const fetchedPost = await client.fetch(POSTS_QUERY)
+            setPost(fetchedPost)
+        }
 
-    const urlParams = useSearchParams()
-    const postId = urlParams.get("post_id")
+        fetchPost()
+    }, [postId])
 
-    const POSTS_QUERY = `*[_type == "post" && _id == "${postId}"][0]{title, short, publishedAt, ...mainImage{...asset-> {url}}, body}`
-   
-    const post = await client.fetch(POSTS_QUERY, {}, options);
+    if (!post) {
+        return (
+            <div className="grid justify-center items-center h-svh">
+                <div class="loader"></div>
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-[680px] m-auto mt-9">
-            <Header h1={post.title} description={post.short} imgUrl={post.url}  />
+            <Header h1={post.title} description={post.short} imgUrl={post.mainImage?.asset?.url} />
             <Body content={post.body} />
         </div>
     )
 }
-
